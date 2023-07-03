22d470062c4346dadfc06583fa39caf6551f5411
Murilo Russo <contato@murilorusso.com.br>
2023-07-03 20:42:35 -0300

    Criada Page buy-details

--- buy-details.php
+++ buy-details.php
@@ -6,7 +6,7 @@
 
          include('connect.php');
 
-         include('protect.php');
+         include('protect-cliente.php');
 
     ?>
 
@@ -20,7 +20,7 @@
 
     </head>
 
-    <body id="buy-details">
+    <body id="page-pedido">
 
         <?php 
 
@@ -34,84 +34,101 @@
             <?php 
 
                 include('left-menu.php');
+                
 
             ?>
 
 
-			<section>
-
-			<?php 
-				
-				
-				
-				$sql_code = "SELECT * FROM pedido WHERE ID='{$_GET['id']}'";	
-				
-				$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
-								
-//				$quantidadePedidos = $sql_query->num_rows;
-				
-				print '<ul class="card">
-				
-					<h1>Detalhes da Compra</h1>
-				
-				';
-				
-				
-
- 				while($row = $sql_query->fetch_object()){
-					
-					
-					if($row->ID_Cliente == $_SESSION['id']){
-						
-						$idProduto = $row->ID_Produtos;
-										
-						$sql_code2 = "SELECT * FROM produtos WHERE id='{$idProduto}'";
-
-						$sql_query2 = $mysqli->query($sql_code2) or die("Falha na execução do código SQL" . $mysqli->error);
-						
-						
-						while($produto = $sql_query2->fetch_object()){
-
-							print '
-
-								<img src="img/products/'.$produto->arquivo.$produto->extensao.'"><h2>'.$produto->nome.'</h2>
-								
-								<p>'.$produto->descricao.'</p>
-
-
-							';
-
-						}
-	//
-						
-					}
-					
-					
-                }    
-				
-				print '</ul>';
-				
-
-			?>
-
-
-
-
-
-
-			</section>
-
-
-
-            <!-- <section>
+            <section>
 
             <?php 
 
-                include('blocks/posts.php');
 
-            ?>
+           
+            if(isset($_GET['id'])){
 
-            </section> -->
+                $idPedido = $_GET['id'];
+
+                $sql_code = "SELECT * FROM pedido WHERE ID=".$idPedido;
+
+                $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+               
+                $pedido = $sql_query->fetch_object();     
+
+                print '<div class="card text-center"> ';
+                
+
+                if(isset($_POST['idPedido'])){
+
+                    $idPedido = $mysqli->real_escape_string($_POST['idPedido']);
+    
+                    $status = $mysqli->real_escape_string($_POST['status']);
+    
+                    $entrega = $mysqli->real_escape_string($_POST['dataEntrega']);
+    
+                    $sql_code = "SELECT * FROM pedido WHERE id=".$idPedido;
+    
+                    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+    
+                    $pedido = $sql_query->fetch_object();
+    
+                    $sql_code = "UPDATE pedido SET Status='{$status}', DataEntrega='{$entrega}' WHERE id=".$idPedido;
+    
+                    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+    
+                    print '<p class="alert-sucess text-center">Dados do Pedido Alterados com sucesso!</p>';
+    
+                }
+
+                print'
+                    
+                    <h2>Pedido: #'.$pedido->ID.'<h2/>
+                                                
+
+                    <p><strong>Data da Compra:</strong> '.$pedido->DataCompra.'</p>
+
+                    <p><strong>Quantidade:</strong> '.$pedido->Quantidade.'</p>
+
+                    <p><strong>Dados da Entrega:</strong> '.$pedido->EnderecoEntrega.'</p>
+
+                    <p><strong>Data da Previsão da Entrega:</strong> '.$pedido->DataPrevisaoEntrega.'</p>
+
+                    <p><strong>Status:</strong> '.$pedido->Status.'</p>
+
+                    <p><strong>Data da Entrega:</strong> '.$pedido->DataEntrega.'</p>
+
+                    
+                    
+                
+                </div>';
+
+
+                
+                $sql_code = "SELECT * FROM produtos WHERE id=".$pedido->ID_Produtos;
+    
+                $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+                $produto = $sql_query->fetch_object();  
+
+                print '<div class="card text-center">                                   
+                            
+                    <h2>Produto: '.$produto->nome.'<h2/>
+                                                
+                    <p><strong>Descrição:</strong> '.$produto->descricao.'</p>
+
+                    <a class="btn btn-primary" href="product.php?id='.$produto->id.'">Ver Produto</a>
+                
+                </div>';
+
+
+
+            }
+
+           
+
+            ?>      
+
+            </section>
 			
 			<?php 
 
@@ -120,10 +137,41 @@
 			?>
 
 
+        </div>    
 
-        </div>
-        
 
+        <script>
+
+            const selectStatus = document.querySelector('select[name="status"]');
+
+            const containerDataEntrega = document.querySelector('#container-data-entrega');
+
+            selectStatus.onchange = () => {
+
+                exibirData();
+
+            }
+
+            function exibirData(){
+
+                if(selectStatus.value === 'Entregue'){
+
+                    containerDataEntrega.style.display = 'flex';
+
+                }
+
+                else{
+
+                    containerDataEntrega.style.display = 'none';
+
+                }
+
+            }
+
+            exibirData();
+
+
+        </script>
 
 
     </body>

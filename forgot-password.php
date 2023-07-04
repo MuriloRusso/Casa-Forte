64985993b66ea54e2c6bbcf714903db7a428e253
Murilo Russo <contato@murilorusso.com.br>
2023-07-04 11:26:44 -0300

    back-end página forgot-password criado, faltando somente fazer o disparo para o e-mail do usuário

--- forgot-password.php
+++ forgot-password.php
@@ -37,29 +37,92 @@
 
                 <?php 
 
+                    if(isset($_POST['email'])){
+
+                        $email = $mysqli->real_escape_string($_POST['email']);
+
+                        // $sql_code = "SELECT * FROM usuario WHERE email=".$email;
+
+
+                        $sql_code = "SELECT * FROM usuario WHERE email = '$email'";
+
+					    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+                        $user = $sql_query->fetch_object();
+                        
+                        $chave = uniqid();
+
+                        $data_envio = date('Y-m-d');
+
+                        $horario = date('H:i:s');
+
+                        if($user){
+
+                            $sql_code = "INSERT INTO forgot_password (id_usuario, chave, data_envio, horario) VALUES ('{$user->id}', '{$chave}', '{$data_envio}', '{$horario}')";
+
+                            $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+                            
+                            
+                            print '
+                            
+                                <p class="alert-sucess text-center">Um e-mail foi enviado para '.$email.' com um link para recuperação da sua senha!</p>
+                            
+                                <a href="login.php" class="btn actions btn-primary">Voltar</a>
+
+
+                            ';
+
+                        }
+
+                        else{
+
+
+                            print '
+                            
+                                <p class="btn-delete btn text-center">Não foi encontrado nenhum usuário com o e-mail '.$email.' vinculado ao mesmo!</p>
+                            
+                                <a href="forgot-password.php" class="btn actions btn-primary">Tentar de Novo</a>
+
+
+                            ';
+
+
+                        }
+
+
+                    }
+
+                    else 
+                    
+                    {
                   
                 ?>
+                        <div class="form-group">
+                            <h2>Preencha o campo abaixo para recuperação da senha!</h2>
+                        </div>
 
+                        <div class="form-group">
 
-                    <div class="form-group">
-                        <h2>Preencha o campo abaixo para recuperação da senha!</h2>
-                    </div>
+                            <label for="email">E-mail:</label>
+                            <input type="email" id="email" name="email" placeholder="Digite seu e-mail aqui" value="<?php /*echo $_POST['email']*/ ?>">
 
-                    <div class="form-group">
+                        </div>
 
-                        <label for="email">E-mail:</label>
-                        <input type="email" id="email" name="email" placeholder="Digite seu e-mail aqui" value="<?php echo $_POST['email'] ?>">
+                        
+                        <div class="form-submit-group flex flex-wrap justify-content-space-betwen">
 
-                    </div>
+                            <input class="btn-primary" type="submit" id="submit" title="Clique aqui para enviar" value="Enviar E-mail de Redefinição de Senha">
 
+                            <p class="text-center full-width">Já Possui conta? <a class="" href="login.php" title="Já possui conta? Clique aqui">Entre Aqui</a></p>
+
+                        </div>
+
+                    <?php 
                     
-                    <div class="form-submit-group flex flex-wrap justify-content-space-betwen">
-
-                        <input class="btn-primary" type="submit" id="submit" title="Clique aqui para enviar" value="Enviar E-mail de Redefinição de Senha">
-
-                        <p class="text-center full-width">Já Possui conta? <a class="" href="login.php" title="Já possui conta? Clique aqui">Entre Aqui</a></p>
-
-                    </div>
+                        }
+                    
+                    ?>
 
 
                 </form>

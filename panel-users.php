51e350dc34a171a4abf82b6eda6126bef64c6f9d
Murilo Russo <contato@murilorusso.com.br>
2023-07-04 13:10:17 -0300

    Adicionado paginação de clientes na página panel-users

--- panel-users.php
+++ panel-users.php
@@ -48,7 +48,7 @@
 				
 				
 				
-				<ul>
+				<!-- <ul>
 					
 					<h2>Administradores:</h2>
 				
@@ -73,21 +73,105 @@
 					<a href="new-user-admin.php" class="btn btn-primary" title="Clique nesse botão para adicionar novo usuário">Adicionar Usuário</a>
 				
 				
-				</ul>
+				</ul> -->
 				
 				
 				
-				<ul>
+				<!-- <ul>
 					
-					<h2>Clientes:</h2>
+					<h2>Clientes:</h2> -->
 				
 					<?php					
 					
-						$sql_code = "SELECT * FROM usuario WHERE papel='cliente'";
+						// $sql_code = "SELECT * FROM usuario WHERE papel='cliente'";
+
+						// $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+						// $quantidadeADMs = $sql_query->num_rows;
+
+						// $sql_code = "SELECT * FROM usuario pedido WHERE papel='cliente'";
+
+						// $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+					
+						// $quantidade = $sql_query->num_rows;
+
+						// if(!isset($_GET['page'])){
+
+						// 	$sql_code = "SELECT * FROM usuario WHERE papel='cliente' ORDER BY id DESC LIMIT 0, 10";
+					
+						// 	$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+					
+						// }
+					
+						// else{
+					
+						// 	$page = intval($_GET['page']) - 1;
+					
+						// 	$sql_code = "SELECT * FROM usuario WHERE papel='cliente' ORDER BY id DESC LIMIT ".$page."0, 10";
+					
+						// 	$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+					
+					
+						// }
+					?>
+
+					<ul>
+					
+						<h2>Administradores:</h2>
+					
+						<?php					
+						
+							$sql_code = "SELECT * FROM usuario WHERE papel='admin'";
+
+							$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+							$quantidadeADMs = $sql_query->num_rows;
+								
+							while($admin = $sql_query->fetch_object()){
+								
+								print '<li class="flex justify-content-space-betwen align-items-center"><div>'.$admin->nome.'</div><div><a href="new-user-admin.php?id='.$admin->id.'" class="btn btn-primary" title="Clique nesse botão para ver mais detalhes">Ver</a></div></li>';
+								
+								
+							}	
+						
+						?>
+						
+					
+						<a href="new-user-admin.php" class="btn btn-primary" title="Clique nesse botão para adicionar novo usuário">Adicionar Usuário</a>
+
+					</ul>
+
+					<ul>
+					
+					<h2>Clientes:</h2>
+
+
+					<?php
+
+						$sql_code = "SELECT * FROM usuario pedido WHERE papel='cliente'";
 
 						$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
 
-						$quantidadeADMs = $sql_query->num_rows;
+						$quantidade = $sql_query->num_rows;
+
+						if(!isset($_GET['page'])){
+
+							$sql_code = "SELECT * FROM usuario WHERE papel='cliente' ORDER BY id DESC LIMIT 0, 10";
+
+							$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+						}
+
+						else{
+
+							$page = intval($_GET['page']) - 1;
+
+							$sql_code = "SELECT * FROM usuario WHERE papel='cliente' ORDER BY id DESC LIMIT ".$page."0, 10";
+
+							$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
+
+
+						}
 							
 						while($cliente = $sql_query->fetch_object()){
 							
@@ -95,9 +179,50 @@
 							
 							
 						}	
+
+
+						if($quantidade > 10){
+
+							$numPages = (intval($quantidade / 10)) + 1;
+						
+				//		$numPages = ceil((intval($quantidadeProdutos / 10)));
+				
+						print '
+				
+						<div class="pagination full-width flex justify-content-center">';
+				
+				
+						for($a = 1; $a < $numPages+1; $a++){
+				
+					//		print '<a href="blog.php?page='.$a.'">'.$a.'</a>';
+				
+							if($a == $_GET['page'] || (!isset($_GET['page']) && $a == 1)){
+				
+								print '<a href="panel-users.php?page='.$a.'" class="btn-pagination-active">'.$a.'</a>';
+				
+							}
+				
+							else{
+				
+								print '<a href="panel-users.php?page='.$a.'">'.$a.'</a>';
+				
+				
+							}
+				
+				
+						}
+				
+				
+						print '</div>';
+							
+				
+					}
+
 					
 					?>
 				
+						
+
 					
 					<a href="new-user-admin.php" class="btn btn-primary" title="Clique nesse botão para adicionar novo usuário">Adicionar Usuário</a>
 				

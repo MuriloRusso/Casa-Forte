8426fbfc79fce11fc7beca8423da01e3b2711303
Murilo Russo <contato@murilorusso.com.br>
2023-07-06 15:07:45 -0300

    tela buy.php puxa os cartoes do cliente em forma de select

--- buy.php
+++ buy.php
@@ -89,8 +89,7 @@
 
                 while($endereco = $sql_query->fetch_object()){
 
-                    print '                             
-                                           
+                    print '                                                    
                         <option>'.$endereco->nome_endereco.'</option>                                                       
 
 
@@ -100,31 +99,163 @@
 
                 print '
                     
-                </select>                    
+                    </select>                    
+
+                            <div class="form-group">
+
+                            <label for="nome">Nome do Endereço:</label>
+                            <input type="text" id="nome" name="nome" value="" required placeholder="Digite nome do seu endereço aqui">
+
+                        </div>
+
+                        
+                        <div class="form-group">
+
+                            <label for="cep">Cep:</label>
+                            <input type="text" id="cep" name="cep" required onkeypress="$(this).mask(`00000-000`)"value="" required placeholder="99999-999">
+                            <a href="#" class=" btn btn-primary" onclick="buscarEndereco()">Buscar Endereço</a>
+
+
+                        </div>
+
+                        
+                        <div class="form-group">
+
+                            <label for="logradouro">Logradouro:</label>
+                            <input type="text" id="logradouro" name="logradouro" value="" required placeholder="Clique em Buscar Endereço para preencher o logradouro" readonly>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="numero">Numero:</label>
+                            <input type="text" id="numero" name="numero" value="" required placeholder="Digite o número do seu endereço aqui">
+
+                        </div>
+
+
+                        <div class="form-group">
+
+                            <label for="bairro">Bairro:</label>
+                            <input type="text" id="bairro" name="bairro" value="" required placeholder="Clique em Buscar Endereço para preencher o bairro" readonly>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="cidade">Cidade:</label>
+                            <input type="text" id="cidade" name="cidade" value="" required placeholder="Clique em Buscar Endereço para preencher a cidade" readonly>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="pais">Pais:</label>
+                            <input type="text" id="pais" name="pais" value="" required placeholder="Clique em Buscar Endereço para preencher o pais" readonly>
+
+                        </div>
+
+
+                            <div class="form-group">
+
+                            <label for="ponto-referencia">Ponto de Referência:</label>
+                            <input type="text" id="ponto-referencia" name="ponto-referencia" value="" required placeholder="Digite o ponto de referência aqui">
+
+                        </div>
+
+
 
                     </div>
 
                 ';
 
 
-                /*$sql_code = "SELECT * FROM usuario WHERE id=".$pedido->ID_Cliente;
-    
+
+                $sql_code = "SELECT * FROM metodosdepagamento WHERE ID_Cliente=".$_SESSION['id'];
+
                 $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
 
-                $user = $sql_query->fetch_object();  
+                print '
+                    
+                    <div class="card text-center">           
+                    
+                    <h2>Pagamento:</h2>
 
-                print '<div class="card text-center">                                   
-                            
-                    <h2>Cliente:</strong> '.$user->nome.'<h2/>
-                                                
-                    <p><strong>E-mail:</strong> '.$user->email.'</p>
-
-                    <p><strong>Telefone:</strong> '.$user->telefone.'</p>
+                    <select name="metodo_de_pagamento">
 
 
-                    <a class="btn btn-primary" href="new-user-admin.php?id='.$user->id.'">Ver Cliente</a>
-                
-                </div>';*/
+
+                ';
+
+                while($metododepagamento = $sql_query->fetch_object()){
+
+                    print '                                                    
+                        <option>'.$metododepagamento->nome_do_cartao.'</option>                                                       
+
+
+                    ';
+
+                }
+
+                print '
+                    
+                    </select>         
+                    
+                    
+                    <div class="form-group">
+
+						<label for="nome">Nome do Cartão:</label>
+						<input type="text" id="nome" name="nome" required placeholder="Digite o nome do seu cartão aqui">
+
+					</div>
+
+					 <div class="form-group">
+
+                        <label for="numero">Numero do Cartão:</label>
+                        <input type="text" id="numero" name="numero" required onkeypress="$(this).mask(`0000 0000 0000 0000`)" placeholder="9999 9999 9999 9999">
+
+                    </div>
+					
+					
+
+                    <div class="form-group">
+
+                        <label for="nome-titular">Nome do Titular do Cartão:</label>
+                        <input type="text" id="nome-titular" name="nome-titular" required placeholder="Digite o nome do titular do cartão aqui">
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <label for="cpf-titular">CPF do Titular do Cartão:</label>
+                        <input type="text" id="cpf-titular" name="cpf-titular" required onkeypress="$(this).mask(`000.000.000-00`)" placeholder="999.999.999-99">
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <label for="bandeira-cartao">Bandeira do Cartão:</label>
+                        <input type="text" id="bandeira-cartao" name="bandeira-cartao" required placeholder="Digite a bandeira do cartão aqui">
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="vencimento">Data de Vencimento do Cartão:</label>
+                        <input type="text" id="vencimento" name="vencimento" onkeypress="$(this).mask(`00/0000`)" placeholder="MM/AAAA" required>
+                    </div>
+					
+					<div class="form-group">
+
+                        <label for="seguranca">Códio de Segurança:</label>
+                        <input type="number" id="seguranca" name="seguranca" required onkeypress="$(this).mask(`000`)" placeholder="999">
+
+                    </div>
+
+				
+                    </div>
+
+                ';
+
 
 
             }
@@ -188,6 +319,29 @@
 
 
             </script>
+
+
+        <script>
+        function buscarEndereco() {
+            var cep = document.getElementById("cep").value;
+            var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+            fetch(url, { method: 'GET' })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                
+                document.getElementById("logradouro").value = data.logradouro;
+                document.getElementById("bairro").value = data.bairro;
+                document.getElementById("cidade").value = data.localidade;
+                document.getElementById("pais").value = "Brasil";
+                
+            })
+            .catch(error => {
+                console.log("Erro ao buscar o endereço:", error);
+            });
+        }
+        </script>
 
 
     </body>

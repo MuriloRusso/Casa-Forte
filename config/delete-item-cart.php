25d15480a49aa07469b983aea203150008f1545f
Murilo Russo <contato@murilorusso.com.br>
2023-07-07 16:33:54 -0300

    Ajustado falha de segurança ao remover carrinho

--- config/delete-item-cart.php
+++ config/delete-item-cart.php
@@ -6,11 +6,41 @@
 
 	$id = $_GET['id'];
 
-	$sql_code = "DELETE FROM carrinho WHERE id=".$id;
+	$sql_code = "SELECT * FROM carrinho WHERE id=".$id;
 
 	$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);
 
-	header("Location: ../carrinho.php");
+	$carrinho = $sql_query->fetch_object();  
+
+	$idCliente = $carrinho->ID_Cliente;
+
+	print $idCliente . '<br>';
+
+	$sql_codeUser = "SELECT * FROM usuario WHERE id=".$idCliente;
+
+	$sql_query = $mysqli->query($sql_codeUser) or die("Falha na execução do código SQL" . $mysqli->error);
+
+	$user = $sql_query->fetch_object();  
+
+
+	if($idCliente == $user->id){
+
+		$sql_code2 = "DELETE FROM carrinho WHERE id=".$id;
+
+		$sql_query = $mysqli->query($sql_code2) or die("Falha na execução do código SQL" . $mysqli->error);
+
+
+		header("Location: ../carrinho.php");
+
+
+	}
+
+	else{
+
+		print 'Você não tem permissão para acessar esse link!';
+
+	}
+
 
 
 ?>

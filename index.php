bd40320c17872aa1d5a63dd688ffbfe8621aae4e
Murilo Russo <contato@murilorusso.com.br>
2023-07-13 17:02:51 -0300

    index-new alterado para index principal

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         ?>
 
 
-        <div class="full-width flex flex-wrap">
+        <div class="full-width">
 
             <?php 
 
@@ -431,121 +431,333 @@
 			
 			
 			
-<!--
-			
-            <section class="flex justify-content-center">
-
-                <div class="seta-carrousel left"><img src="img/carrousel/icons/circulo-de-flecha.png" class="espelhar" alt="<"></div>  
-
-                    <div class="overflow-hidden">
-
-                        <div class="container-carrousel">
-                            
-                            <div class="carrousel-item active flex">
-
-                                    <?php 
-
-                                        include('blocks/carrousel/mais-vendidos.php');
-
-                                    ?>
-
-                            </div>
-
-                            <div class="carrousel-item flex">
-
-                                    <?php 
-
-                                        include('blocks/carrousel/mais-vendidos.php');
-
-                                    ?>
-
-                            </div>
-
-
-                            <div class="carrousel-item flex">                    
-
-                                    <?php 
-
-                                        include('blocks/carrousel/mais-vendidos.php');
-
-                                    ?>
-                            
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                <div class="seta-carrousel right"><img src="img/carrousel/icons/circulo-de-flecha.png" alt=">"></div> 
-
-            </section>
-			
-			
-            <script src="js/carrousel.js"></script>
-			
--->
 			<?php
 			
 			if($_SESSION['papel'] == 'cliente' || !isset($_SESSION['papel'])){
 				
 			?>
 			
-			
-			<section class="flex justify-content-center carrousel-scroll">
+
+				<section class="banner flex align-items-center justify-content-center">
+
+					<div>
+
+						<h1>CASA FORTE PROJETOS E  SERVIÇOS DE SEGURANÇA</h1>
+
+						<h2>Segurança, Instalação e  Monitoramento de Câmeras e Alarmes</h2>
+
+					</div>
+
+				</section>
+
+				<section class="up">
+					
+					<div id="container-icons">
+
+						<div class="col-33 flex item">
+
+							<img src="" alt="">
+
+							<div>
+								<h2>PROTECTION</h2>
+
+								<p>PRIVATE SECURITY</p>
+
+							</div>
+
+						</div>
+
+						<div class="col-33 flex item">
+
+							<img src="" alt="">
+
+							<div>
+								<h2>MONITORING</h2>
+
+								<p>BANKING SECTOR</p>
+
+							</div>
+
+						</div>
+
+						<div class="col-33 flex item">
+
+							<img src="" alt="">
+
+							<div>
+								<h2>FINGERPRINT</h2>
+
+								<p>EDUCATION INSTITUTE</p>
+
+							</div>
+
+						</div>
+
+					</div>
+					
+					
+				</section>
+
+				<section class="margin-0">
+
+					<div class="text-center boxed">
+
+						<h2 class="font-primary"> QUEM SOMOS...</h2>
+
+						<h3 class="font-secondary">Nossa Empresa, foi criada e desenvolvida especificamente para garantir segurança e tranquilidade as familias, bem como as Empresas.
+
+ 
+
+Nossa equipe e formada por profissionais altamente qualificados, equipamentos de ultima geração e tecnologia de ponta.
+
+ 
+
+Desenvolvimento de projetos individualizado e especifico a necessidade de cada cliente.</h3>
+
+					</div>
+
+					<div class="boxed flex flex-wrap">
+
+						<div class="col-50">
+
+							<div class="bg" id="bg-1"></div>
+
+							<div class="coment">
+
+								<p class="font-primary">SECURITY PROFESSIONAL </p>
+								<p class="font-secondary">Experts and Staff</p>
+
+							</div>
+
+
+						</div>
+
+						<div class="col-50">
+
+							<div class="bg" id="bg-2"></div>
+
+							<div class="coment">
+
+								<p class="font-primary">EXPLORE THE TECHNIQUE</p>
+								<p class="font-secondary">BODYGUARD SECURITY</p>
+
+							</div>
+
+						
+						</div>
+
+
+
+					</div>
+
+				</section>
+
+
+				<section class="boxed">
+
+					<div class="text-center">
+
+						<h2 class="font-primary">SERVIÇOS</h2>
+
+						<h3 class="font-secondary">CÂMERAS DE SEGURANÇA</h3>
+
+					</div>
+
+					<div class="flex flex-wrap">
+
+						<div class="col-33 text-center">
+
+							<div class="card-model-2">
+
+							<img src="img/home/icons/servicos/responsive-design.png" alt="">
+
+								<h2>Câmeras com acesso em tempo real via celular, computador ou tablet</h2>
+
+								<p>Lorem ipsum dolor ame elit, sed do eiusmod tempor incididunt</p>
+
+							</div>
+
+						</div>
+
+						<div class="col-33 text-center">
+
+							<div class="card-model-2">
+
+							<img src="img/home/icons/servicos/face.png" alt="">
+
+								<h2>Câmeras com Sistema de identificação facial</h2>
+
+								<p>Lorem ipsum dolor ame elit, sed do eiusmod tempor incididunt</p>
+
+							</div>
+
+						</div>
+
+
+						<div class="col-33 text-center">
+
+							<div class="card-model-2">
+
+							<img src="img/home/icons/servicos/movement.png" alt="">
+
+								<h2>Câmeras com sensor de movimento e sensor de calor</h2>
+
+								<p>Lorem ipsum dolor ame elit, sed do eiusmod tempor incididunt</p>
+
+							</div>
+
+						</div>
+
+
+					</div>
+					
+
 				
-				<div class="seta-carrousel left"><img src="img/carrousel/icons/circulo-de-flecha.png" class="espelhar" alt="<"></div>  
+				</section>
+
+						
+				<div class="text-center boxed">
+
+					<h2 class="font-primary">OUR SINGIN 1987.</h2>
+
+					<h3 class="font-secondary">DISCOVER THE WORLD OF BEST CCTV SAFETY.</h3>
+
+				</div>
+		
+
+
+				<section class="flex justify-content-center carrousel-scroll margin-0">
+
 				
-					 <div class="carrousel-scroll-itens">
+					<div class="seta-carrousel left"><img src="img/carrousel/icons/circulo-de-flecha.png" class="espelhar" alt="<"></div>  
+					
+						<div class="carrousel-scroll-itens">
 
-						<?php 
+							<?php 
 
-							include('blocks/carrousel-scroll/carrousel-scroll-item-1.php');
+								include('blocks/carrousel-scroll/carrousel-scroll-item-1.php');
 
-						?>
+							?>
 
-						 
-                   	</div>
+							
+						</div>
 
-                <div class="seta-carrousel right"><img src="img/carrousel/icons/circulo-de-flecha.png" alt=">"></div> 
-			
-			
-			</section>
-			
-			<script src="js/carrousel-scroll.js"></script>
+					<div class="seta-carrousel right"><img src="img/carrousel/icons/circulo-de-flecha.png" alt=">"></div> 
+				
+				
+				</section>
+				
+				<script src="js/carrousel-scroll.js"></script>
 
-            <section>
+						
+				<section class="margin-0">
+						
+					<div class="text-center boxed">
 
-                <?php 
+						<h2 class="font-primary">HOW WE WORK</h2>
 
-                    include('blocks/products.php');
+						<h3 class="font-secondary">SECURITY SURVEILLANCE SYSTEMS</h3>
 
-                ?>
+					</div>
 
-            </section>
+					<div class="flex flex-wrap">
 
-            <!-- <section>
+						<div class="col-33">
 
-            <?php 
+							<div class="card-model-3 bg-1">
 
-                include('blocks/posts.php');
+								<div class="content">
 
-            ?>
+									<h3>RECONHECIMENTO FACIAL</h3>
+									<p>Sistema e equipamentos com sensores de reconhecimento facial, através de cadastros dos moradores dos condominios, ou funcionários das Empresas,  ou ainda de imagens disponibilizadas nas redes sociais e cadastros oficiais.
 
-            </section> -->
+</p>
+
+								</div>
 
 
-			 <section>
+							</div>
 
-            <?php 
 
-                include('blocks/more-sell.php');
+						</div>
 
-            ?>
+						<div class="col-33">
 
-            </section> 
-			
-			
-			
+							<div class="card-model-3 bg-2">
+
+								<div class="content">
+									<h3>CONTROLADORES DE ACESSO RESIDENCIAL  E DE PORTARIA PREDIAL</h3>
+									<p>Sistema de controle de acesso de moradores e de funcionários de Empresas, através da biometria digital, facial ou de leitura de retina.
+
+</p>
+
+
+								</div>
+
+
+							</div>
+
+							
+						</div>
+
+
+						<div class="col-33">
+
+							<div class="card-model-3 bg-3">
+
+								<div class="content">
+
+									<h3> SISTEMA DE MONITORAMENTO E ALARME</h3>
+
+									<p>Nosso Sistema de Alarme Residencial possui equipamentos de última geração que garantem o monitoramento 24 horas por dia da sua residência. Com o Monitoramento Residencial  você pode contar com a melhor proteção para seu lar, da sua família e de seu patrimônio</p>
+
+								</div>
+
+
+
+							</div>
+
+							
+						</div>
+
+
+					</div>
+
+
+
+				</section>
+
+
+				<section>
+
+					<div class="boxed flex flex-wrap">
+
+						<div class="col-50 flex align-items-center">
+
+							<div class="text-pad">
+								<h2 class="font-primary">Nossos projetos</h2>
+								<h3 class="font-secondary">Projetos personalizados que e atendem casas, apartamentos, sobrados etc...</h3>
+
+								<p>Nossos projetos são personalizados e atendem casas, apartamentos, sobrados etc. O monitoramento e alarme possibilita através de sensores de presença, sensores perimetrais, câmeras de segurança e botões de pânico garantindo a detecção de intrusos, que permite uma atuação de maneira eficaz em qualquer situação de emergência.</p>
+
+							</div>	
+					
+						</div>
+
+
+						<div class="col-50">
+
+							<img src="img/home/nossos-projetos.jpg" alt="">
+
+						</div>
+
+
+					</div>
+
+				</section>
+					
+
+
 			<?php
 			
 			}
